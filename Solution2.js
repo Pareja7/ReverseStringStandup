c80f323f74a574c92a761b10304c6df30cf8ca87
Pareja7 <Pareja7@users.noreply.github.com>
2021-10-08 18:26:01 +0000

    Added For Loop (reverse string solution)

--- Solution2.js
+++ Solution2.js
@@ -1,9 +1,11 @@
-// Complete the solution so that it reverses the string.
+// FOR LOOP: Complete the solution so that it reverses the string.
 function reverseStr(){
-// return str.split('').reverse().join('')
+
 let newStr = ''
 for(let i = str.length -1){
   newStr += str[i]
   }
 return newStr
+
+reverseStr('world')
 }
