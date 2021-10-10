ab3acff505264c01ea2679130b971f07b7c6f372
Pareja7 <Pareja7@users.noreply.github.com>
2021-10-10 22:55:53 +0000

    Spread operator solution

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Chaining in-built methods
 
 //'wolrd' => 'dlrow'
 function reverseStr(str) {
@@ -5,4 +6,16 @@ function reverseStr(str) {
 }
 
 console.log(reverseStr('world'),'dlrow')
+
+~~~~~~~~~~~~
+
+//Using the spread operator ..., like the .split() method spreads the characters of the string into array elements or sub-strings)
+
+function reverseStr(str){
+  return [...str].reverse().join('')
+
+}
+
+console.log(reverseStr('world'))
+
   
