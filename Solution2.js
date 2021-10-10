47d88897fc8e33a9b494e4733335cd4a90e83762
Pareja7 <Pareja7@users.noreply.github.com>
2021-10-10 22:13:27 +0000

    Added comments

--- Solution2.js
+++ Solution2.js
@@ -2,9 +2,12 @@
 function reverseStr(){
 
 let newStr = ''
+//Start looping from end of string 
 for(let i = str.length -1){
+  //Pass each looped through sub-string character to the newStr varialbe.
   newStr += str[i]
   }
+  
 return newStr
 
 reverseStr('world')
